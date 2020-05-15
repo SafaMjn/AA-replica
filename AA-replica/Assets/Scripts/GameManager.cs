261fd8b869ee219495d0eb60b356a09c82fe1051
Safa Morjane <safa.morjane@gmail.com>
2020-05-15 02:18:51 +0100

    Gameplay added

---
+++ AA-replica/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    private bool isGameOver = false;
+    public Animator animator;
+
+    public  int pinCount ;
+    public TMP_Text score_txt;
+
+
+    void Start()
+    {
+        pinCount = 0;
+        score_txt.text = pinCount.ToString();
+    }
+
+    public void EndGame()
+    {
+        Rotation_ctr.Instance.enabled = false;
+        PinSpawner_ctr.Instance.enabled = false;
+
+        isGameOver = true;
+
+        animator.SetTrigger("EndGame");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void SetScore()
+    {
+        score_txt.text = pinCount.ToString();
+    }
+}
